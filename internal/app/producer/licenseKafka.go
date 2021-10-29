78f0e2d941ff9083544e1b8d4d121a1c1a398dc0
achiglintsev <achiglintsev@avito.ru>
2021-10-30 00:28:02 +0300

    TASK-2 add third test ProducerTestSuite::TestStart & ProducerTestSuite::TestEventChanRead

--- internal/app/producer/licenseKafka.go
+++ internal/app/producer/licenseKafka.go
@@ -2,12 +2,13 @@ package producer
 
 import (
 	"context"
+	"github.com/gammazero/workerpool"
 	"github.com/ozonmp/lic-license-api/internal/app/repo"
 	"github.com/ozonmp/lic-license-api/internal/app/sender"
-	workerpool "github.com/ozonmp/lic-license-api/internal/app/worker_pool"
+
+	//workerpool "github.com/ozonmp/lic-license-api/internal/app/worker_pool"
 	"github.com/ozonmp/lic-license-api/internal/model/license"
 	"sync"
-	"time"
 )
 
 type LicenseProducer interface {
@@ -16,59 +17,69 @@ type LicenseProducer interface {
 }
 
 type licenseProducer struct {
-	n       uint64
-	timeout time.Duration
-
-	sender sender.LicenseEventSender
-	repo   repo.LicenseEventRepo
-	events <-chan license.LicenseEvent
-
-	workerPool workerpool.WorkerLicPool
-
-	wg   *sync.WaitGroup
-	done chan bool
+	workerCount uint64
+	//timeout     time.Duration
+	sender     sender.LicenseEventSender
+	workerPool *workerpool.WorkerPool //WorkerLicPool
+	repo       repo.LicenseEventRepo
+	events     <-chan license.LicenseEvent
+	wg         *sync.WaitGroup
+	done       chan bool
 }
 
 func NewKafkaLicenseProducer(
-	n uint64,
+	workerCount uint64,
 	sender sender.LicenseEventSender,
 	events <-chan license.LicenseEvent,
-	workerPool workerpool.WorkerLicPool,
+	workerPool *workerpool.WorkerPool,
+	repo repo.LicenseEventRepo,
 ) LicenseProducer {
 
 	wg := &sync.WaitGroup{}
 	done := make(chan bool)
 
 	return &licenseProducer{
-		n:          n,
-		sender:     sender,
-		events:     events,
-		workerPool: workerPool,
-		wg:         wg,
-		done:       done,
+		workerCount: workerCount,
+		sender:      sender,
+		events:      events,
+		workerPool:  workerPool,
+		wg:          wg,
+		done:        done,
+		repo:        repo,
+	}
+}
+
+func (p *licenseProducer) produceEvent(event license.LicenseEvent) {
+	if err := p.sender.Send(&event); err != nil {
+		// update
+		p.workerPool.Submit(func() {
+			p.repo.Unlock([]uint64{event.ID})
+		})
+	} else {
+		// clean
+		p.workerPool.Submit(func() {
+			p.repo.Remove([]uint64{event.ID})
+		})
+	}
+}
+
+func (p *licenseProducer) consumeEvents(ctx context.Context) {
+	defer p.wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case event := <-p.events:
+			p.produceEvent(event)
+		}
 	}
 }
 
 func (p *licenseProducer) Start(ctx context.Context) {
-	for i := uint64(0); i < p.n; i++ {
+	//ctx context.Context
+	for i := uint64(0); i < p.workerCount; i++ {
 		p.wg.Add(1)
-		go func() {
-			defer p.wg.Done()
-			for {
-				select {
-				case event := <-p.events:
-					if event.Type == license.Created {
-						if err := p.sender.Send(&event); err != nil {
-							p.workerPool.Update(event)
-						} else {
-							p.workerPool.Clean(event)
-						}
-					}
-				case <-ctx.Done():
-					return
-				}
-			}
-		}()
+		go p.consumeEvents(ctx)
 	}
 }
 
