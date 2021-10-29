0b22ef2f838af0fea3db6b29b8a204e763cd2851
achiglintsev <achiglintsev@avito.ru>
2021-10-29 23:48:32 +0300

    TASK-2 run success consumer tests. mockgen work

--- internal/mockgen.go
+++ internal/mockgen.go
@@ -1,7 +1,5 @@
 package internal
 
-//go:generate mockgen -destination=./mocks/repo_mock.go -package=mocks github.com/ozonmp/lic-license-api/internal/app/repo EventRepo
 //go:generate mockgen -destination=./mocks/repo_license_mock.go -package=mocks github.com/ozonmp/lic-license-api/internal/app/repo LicenseEventRepo
-//go:generate mockgen -destination=./mocks/sender_mock.go -package=mocks github.com/ozonmp/lic-license-api/internal/app/sender EventSender
 //go:generate mockgen -destination=./mocks/sender_license_mock.go -package=mocks github.com/ozonmp/lic-license-api/internal/app/sender LicenseEventSender
-//go:generate mockgen -destination=./mocks/worker_pool_mock.go -package=mocks github.com/ozonmp/lic-license-api/internal/app/sender WorkerLicPool
+//go:generate mockgen -destination=./mocks/worker_pool_mock.go -package=mocks github.com/ozonmp/lic-license-api/internal/app/worker_pool WorkerLicPool
